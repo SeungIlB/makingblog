4df0b19bf964acac669bc9423d027ea430e5823c
SeungIlB <piseouk787@gmail.com>
2025-07-08 15:17:39 +0900

    Docs: 주석추가

--- src/designpattern/creationalpattern/FactoryMethod.java
+++ src/designpattern/creationalpattern/FactoryMethod.java
@@ -1,47 +1,65 @@
 package designpattern.creationalpattern;
 
-// Product 인터페이스
-interface Animal{
+// Product 인터페이스: 생성될 객체들의 공통 인터페이스
+interface Animal {
     void speak();
 }
 
-// Concrete Product
-class Dog implements Animal{
-    public void speak(){
+// Concrete Product: 실제 생성될 객체들
+class Dog implements Animal {
+    @Override
+    public void speak() {
         System.out.println("멍멍!");
     }
 }
 
-class Cat implements Animal{
-    public void speak(){
+class Cat implements Animal {
+    @Override
+    public void speak() {
         System.out.println("야옹~");
     }
 }
 
-// Creator
-abstract class AnimalFactory{
+// Creator 추상 클래스: 팩토리 메서드를 선언
+abstract class AnimalFactory {
+    // 팩토리 메서드: 객체 생성을 담당하는 메서드 (하위 클래스에서 구현)
     public abstract Animal createAnimal();
+
+    // 팩토리 메서드를 활용하는 로직 (선택 사항)
+    public void doSomethingWithAnimal() {
+        Animal animal = createAnimal(); // 하위 클래스가 어떤 동물을 만들지 결정
+        animal.speak();
+    }
 }
-class DogFactory extends AnimalFactory{
-    public Animal createAnimal(){
+
+// Concrete Creator: 팩토리 메서드를 구현하여 특정 Product 객체 생성
+class DogFactory extends AnimalFactory {
+    @Override
+    public Animal createAnimal() {
         return new Dog();
     }
 }
 
-class CatFactory extends AnimalFactory{
-    public Animal createAnimal(){
+class CatFactory extends AnimalFactory {
+    @Override
+    public Animal createAnimal() {
         return new Cat();
     }
 }
 
+// 테스트 코드
 public class FactoryMethod {
     public static void main(String[] args) {
-        AnimalFactory factory = new DogFactory();
-        Animal dog = factory.createAnimal();
+        AnimalFactory dogFactory = new DogFactory();
+        Animal dog = dogFactory.createAnimal();
         dog.speak(); // 멍멍!
 
-        factory = new CatFactory();
-        Animal cat = factory.createAnimal();
+        AnimalFactory catFactory = new CatFactory();
+        Animal cat = catFactory.createAnimal();
         cat.speak(); // 야옹!
+
+        System.out.println("\n--- 팩토리 메서드를 활용한 추가 로직 ---");
+        dogFactory.doSomethingWithAnimal();
+        catFactory.doSomethingWithAnimal();
     }
 }
